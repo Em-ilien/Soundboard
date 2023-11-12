790ab5ed490a9cf6c3e4be6bdbba932d3706bc30
Emilien Cosson <emilien@em-ilien.fr>
2023-11-12 14:18:25 +0100

    refactor soundboard extract component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Soundboard from "./Soundboard.svelte";
+
   import { soundboards } from "./../soundboardsStore.js";
   import NewModal from "./NewModal.svelte";
   import SoundboardsList from "./SoundboardsList.svelte";
@@ -44,11 +46,7 @@
     on:switchSoundboard={onSwitchSoundboard}
   />
 
-  <div class="soundboard">
-    {#each $soundboards[currentSoundboardIndex].sounds as sound}
-      <SoundButton {sound} on:play={onPlay} />
-    {/each}
-  </div>
+  <Soundboard {currentSoundboardIndex} />
 
   <New on:new={onNew} />
 
@@ -75,34 +73,5 @@
     flex-direction: row;
     justify-content: space-between;
     align-items: flex-start;
-  }
-
-  .soundboard {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: stretch;
-    gap: 0 1.5em;
-    width: calc(100vw - 3em);
-    max-height: 100vh;
-    overflow-y: scroll;
-    padding: 1em;
-    user-select: none;
-  }
-
-  :global(.modal) ul {
-    list-style: none;
-    display: flex;
-    flex-direction: row;
-    padding: 0.5em 0;
-  }
-
-  :global(.modal) li {
-    padding: 0 0.5em;
-    border-right: 1px solid #7d7d7d;
-  }
-  :global(.modal) li:last-child {
-    border-right: none;
   }
 </style>
---
+++ src/routes/Soundboard.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { soundboards } from "../soundboardsStore.js";
+  import SoundButton from "./SoundButton.svelte";
+
+  export let currentSoundboardIndex = 0;
+
+  function onPlay(event) {
+    console.log(event.detail);
+    const sound = new Audio(event.detail);
+    sound.play();
+  }
+</script>
+
+<div class="soundboard">
+  {#each $soundboards[currentSoundboardIndex].sounds as sound}
+    <SoundButton {sound} on:play={onPlay} />
+  {/each}
+</div>
+
+<style>
+  .soundboard {
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: stretch;
+    gap: 0 1.5em;
+    width: calc(100vw - 3em);
+    max-height: 100vh;
+    overflow-y: scroll;
+    padding: 1em;
+    user-select: none;
+  }
+</style>
